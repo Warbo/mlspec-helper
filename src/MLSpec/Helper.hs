ec52fefd59495f29823d742d6ddf469bf0288532
Chris Warburton <chriswarbo@gmail.com>
2015-12-14 07:04:28 +0000

    Try runtime type checking instead of ifCxt

--- src/MLSpec/Helper.hs
+++ src/MLSpec/Helper.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE RankNTypes, FlexibleInstances, FlexibleContexts, KindSignatures, ScopedTypeVariables, MultiParamTypeClasses, TemplateHaskell #-}
+{-# OPTIONS_GHC -fdefer-type-errors #-}
 module MLSpec.Helper where
 
 import Data.Char
@@ -12,11 +13,21 @@ import Test.QuickCheck.All
 import Test.QuickSpec
 import Test.QuickSpec.Signature
 import Test.QuickSpec.Utils.Typed
+import Data.Constraint
+import Control.Exception
+import System.IO.Unsafe
 
--- Added these to make testing easier
-import Data.Word
+getArb :: (Typeable a) => a -> Gen a
+getArb x = unsafePerformIO $ do
+    res <- try $ evaluate $ arb' Dict x
+    case res of
+        Right y             -> return y
+        Left  (ErrorCall _) -> return (error ("No arbitrary for " ++ (show (typeRep [x]))))
 
-mkIfCxtInstances ''Arbitrary
+arb' :: (Dict (Arbitrary a)) -> a -> Gen a
+arb' Dict _ = arbitrary
+
+--mkIfCxtInstances ''Arbitrary
 
 mono = fmap vToC . monomorphic
 
@@ -27,12 +38,14 @@ vToC x        = x
 isC n = let (c:_) = nameBase n
          in isUpper c || c `elem` ":["
 
+{-
 {-# NOINLINE getArb #-}
 getArb :: forall a. (Typeable a, IfCxt (Arbitrary a)) => a -> Gen a
 getArb x = ifCxt (Proxy::Proxy (Arbitrary a))
   arbitrary
   (unsafePerformIO (do hPutStrLn stderr ("No arbitrary for " ++ show (typeRep [x]))
                        return (error "No arbitrary instance")))
+-}
 
 addVars :: Sig -> Sig
 addVars sig = signature (sig : vs)
@@ -42,13 +55,3 @@ addVars sig = signature (sig : vs)
                 Some w `notElem`  variableTypes sig]
         names x = let n = show (typeRep [x])
                    in [n ++ "1", n ++ "2", n ++ "3"]
-
-{-
-isArbitrary :: Name -> Bool
-isArbitrary n =
-
-arbInstances ::
-
-makeVars :: Name -> ExpQ
-makeVars n = reify n
--}
