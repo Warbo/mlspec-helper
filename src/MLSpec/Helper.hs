dbca4aafd9d5733e33a05afa527449ab8dfde16f
Chris Warburton <chriswarbo@gmail.com>
2016-01-25 17:29:45 +0000

    Abandon runtime type name extraction hacks

--- src/MLSpec/Helper.hs
+++ src/MLSpec/Helper.hs
@@ -1,5 +1,8 @@
 {-# LANGUAGE RankNTypes, FlexibleInstances, FlexibleContexts, KindSignatures, ScopedTypeVariables, TemplateHaskell #-}
-module MLSpec.Helper where
+module MLSpec.Helper
+       ( module MLSpec.Helper
+       , module IfCxt
+       ) where
 
 import Control.Monad
 import Data.Char
@@ -8,6 +11,7 @@ import Data.Typeable
 import Data.Word
 import IfCxt
 import Language.Haskell.TH.Syntax
+import RuntimeArbitrary
 import System.IO
 import System.IO.Unsafe
 import Test.QuickCheck
@@ -18,27 +22,6 @@ import Test.QuickSpec.Utils.Typed
 import Data.Constraint
 import Control.Exception
 
-{-
-getArb :: (Typeable a) => a -> Gen a
-getArb x = unsafePerformIO $ do
-    res <- try $ evaluate $ arb' Dict x
-    case res of
-        Right y             -> return y
-        Left  (ErrorCall _) -> return (error ("No arbitrary for " ++ (show (typeRep [x]))))
-
-arb' :: (Dict (Arbitrary a)) -> a -> Gen a
-arb' Dict _ = arbitrary
-
-haveArb :: (Typeable a) => a -> Bool
-haveArb x = unsafePerformIO $ do
-    res <- try $ evaluate $ arb' Dict x
-    case res of
-        Right y             -> return True
-        Left  (ErrorCall _) -> return False
--}
-
-mkIfCxtInstances ''Arbitrary
-
 mono = fmap vToC . monomorphic
 
 vToC (VarE n) = if isC n then ConE n else VarE n
@@ -48,68 +31,6 @@ vToC x        = x
 isC n = let (c:_) = nameBase n
          in isUpper c || c `elem` ":["
 
-getArbGen :: forall a. (Typeable a, IfCxt (Arbitrary a)) => Witnessed a -> [Gen a]
-getArbGen x = ifCxt (Proxy::Proxy (Arbitrary a))
-                    [arbitrary]
-                    (listArb x)
-
-{-
-getArb :: (Typeable a) => a -> Gen a
-getArb = extM (extM (extM (extM arbNope arbBool) arbMaybe) arbList) arbM
-  where arbNope :: Typeable a => a -> Gen a
-        arbNope x = error ("No generator for " ++ show (typeRep [x]))
-        arbBool :: Bool -> Gen Bool
-        arbBool _ = arbitrary
-        arbMaybe :: Maybe Char -> Gen (Maybe Char)
-        arbMaybe _ = arbitrary
-        arbList :: [Char] -> Gen [Char]
-        arbList _ = arbitrary
-        arbM :: Maybe Word8 -> Gen (Maybe Word8)
-        arbM _ = arbitrary
--}
-
-newtype GenList a = GL [Gen a]
-
-instance Functor GenList where
-  fmap f (GL xs) = GL (fmap (fmap f) xs)
-
-instance Applicative GenList where
-  pure a = GL [return a]
-  GL xs <*> GL ys = GL (map (<*>) xs <*> ys)
-
-instance Monad GenList where
-  return = pure
-  GL xs >>= f = GL [a]
-    where a = join b
-          b = fmap (\(GL ys) -> oneof ys) c
-          c = fmap f d
-          d = oneof xs
-
-gConcat [] = []
-gConcat (GL xs:ys) = xs ++ gConcat ys
-
-lstArb :: (Typeable a) => a -> GenList a
-lstArb = extM (extM (extM (extM arbNope arbBool) arbMaybe) arbList) arbM
-
-arbNope :: Typeable a => a -> GenList a
-arbNope x = GL []
-
-arbBool :: Bool -> GenList Bool
-arbBool _ = GL [arbitrary]
-
-arbMaybe :: Maybe Char -> GenList (Maybe Char)
-arbMaybe _ = GL [arbitrary]
-
-arbList :: String -> GenList String
-arbList _ = GL [arbitrary]
-
-arbM :: Maybe Word8 -> GenList (Maybe Word8)
-arbM _ = GL [arbitrary]
-
-listArb :: (Typeable a) => Witnessed a -> [Gen a]
-listArb x = case lstArb (witness x) of
-  GL xs -> xs
-
 addVars :: Sig -> Sig
 addVars sig = signature (sig : vs)
   where vs :: [Sig]
@@ -117,7 +38,6 @@ addVars sig = signature (sig : vs)
                   Some w <-        argumentTypes sig
                 , Some w `notElem` variableTypes sig
                 , gen    <-        getArbGen w
-                --, haveArb (witness w)
                 ]
         names w = let n = show (witnessType w)
                    in ["var(" ++ n ++ ", 1)",
--- test/Main.hs
+++ test/Main.hs
@@ -1,68 +1,93 @@
+{-# LANGUAGE TemplateHaskell, FlexibleInstances #-}
+
 module Main where
 
 import           Data.List
 import           Data.Typeable
 import           MLSpec.Helper
+import           RuntimeArbitrary
 import           Test.QuickCheck
+import           Test.QuickCheck.Arbitrary
 import           Test.QuickCheck.Property
 import           Test.QuickSpec
 import           Test.QuickSpec.Signature
 import           Test.QuickSpec.Term
+import           Test.QuickSpec.Utils.Typeable (unTypeRep)
 import           Test.QuickSpec.Utils.Typed
 import           Test.QuickSpec.Utils.TypeRel
 import           Test.Tasty             (defaultMain, testGroup, localOption)
 import           Test.Tasty.QuickCheck
 
+mkIfCxtInstances ''Arbitrary
+
 main = defaultMain $ testGroup "All tests" [
-    testProperty "Have arbitrary Bools"            haveArbitraryBools
-  , testProperty "Have arbitrary Integers"         haveArbitraryIntegers
-  , testProperty "Arbitrary Integers are distinct" integersAreDistinct
-  , testProperty "Can generate nonzero Integers"   getNonZeroIntegers
+    testProperty "Have Bool generator"             haveBoolGen
+  , testProperty "Have Integer generator"          haveIntGen
+  , testProperty "Bool generator works"            haveArbitraryBools
+  , testProperty "Integer generator works"         haveArbitraryInts
+  , testProperty "Arbitrary Integers are distinct" intsAreDistinct
+  , testProperty "Arbitrary Bools are distinct"    boolsAreDistinct
+  , testProperty "Can generate nonzero Integers"   getNonZeroInts
   , testProperty "Can check for variable types"    varCheckingWorks
-  , testProperty "Bool variables added"            getBoolVars
-  --, testProperty "Integer variables added"         getIntegerVars
+  , testProperty "Can get argument types out"      haveArgTypes
+  , testProperty "Argument types are correct"      checkArgTypes
+  , testProperty "Can get variable types out"      haveVariableTypes
   ]
 
--- Bools have a hard-coded special case generator
-haveArbitraryBools = if null gens
-                        then failure
-                        else forAll (vectorOf 100 (oneof gens)) distinctBools
-  where gens = getArbGen (Witness (undefined :: Bool))
-        distinctBools xs = True `elem` xs && False `elem` xs
+haveBoolGen = not (null boolGens)
+haveIntGen  = not (null intGens)
 
--- Integer isn't hard-coded, but it should be picked up by ifcxt
-haveArbitraryIntegers = if null gens
-                           then failure
-                           else forAll (vectorOf 100 (oneof gens))
-                                       distinctIntegers
-  where gens = getArbGen (Witness (undefined :: Integer))
+-- `getArbGen foo !! bar` should behave the same as `arbitrary`. We check both,
+-- to ensure the test is working as intended.
+haveArbitraryBools = not (null boolGens) ==> distinctVals boolGen
+boolsAreDistinct   =                         distinctVals (arbitrary :: Gen Bool)
 
--- getArbGen should return arbitrary, so we should get the same behaviour for
--- each
-integersAreDistinct = forAll (vectorOf 100 arbitrary)
-                             distinctIntegers
+haveArbitraryInts = not (null intGens) ==> distinctVals intGen
+intsAreDistinct   =                        distinctVals (arbitrary :: Gen Integer)
 
-getNonZeroIntegers :: Integer -> Bool
-getNonZeroIntegers 0 = discard
-getNonZeroIntegers n = True
-
-getBoolVars = sig `hasVarType` (undefined :: Bool)
-  where sig = addVars (signature ["not" `fun1` not])
-
-getIntegerVars = sig `hasVarType` (undefined :: Integer)
-  where sig = addVars (signature ["fromInteger" `fun1` (fromInteger :: Integer -> Int)])
+getNonZeroInts :: Integer -> Bool
+getNonZeroInts 0 = discard
+getNonZeroInts n = True
 
 varCheckingWorks :: Int -> [String] -> Bool
 varCheckingWorks x names = if null names then not found else found
   where sig = signature [gvars names (return x)]
         found = hasVarType sig (0 :: Int)
 
+haveArgTypes = 1 === length [() | Some w <- argumentTypes boolSig']
+
+checkArgTypes = typeRep [True] `elem` typeReps
+  where argTypes = argumentTypes boolSig'
+        typeReps = map (unTypeRep . witnessType) argTypes
+
+haveVariableTypes s = expected === length [() | Some w <- variableTypes sig]
+  where expected = if null s then 0 else 1
+        sig      = gvars s (return True)
+
+-- Helpers
+
 hasVarType :: (Typeable a) => Sig -> a -> Bool
 hasVarType sig x = let vars  = variables sig
                        found = Test.QuickSpec.Utils.TypeRel.lookup x vars
                     in not (null found)
 
-distinctIntegers :: [Integer] -> Bool
-distinctIntegers xs = (length (nub xs) >= 2) || discard
+-- Make sure a `Gen a` isn't just `return foo` (we need to resize it first)
+distinctVals :: (Eq a, Show a) => Gen a -> Property
+distinctVals gen = forAll genList distinct
+  where genList     = resize 1000 $ vectorOf 100 gen
+        distinct xs = length (nub xs) >= 2
+
+boolSig' = signature ["not" `fun1` not]
+boolSig  = addVars boolSig'
+
+intSig' = signature ["fromInteger" `fun1` (fromInteger :: Integer -> Int)]
+intSig  = addVars intSig'
+
+boolGens = getArbGen (Witness (undefined :: Bool))
+intGens  = getArbGen (Witness (undefined :: Integer))
+
+-- We should have (at least) one generator; might as well test all of them
+boolGen = oneof boolGens
+intGen  = oneof intGens
 
 failure = MkProperty (return (MkProp (MkRose failed [])))
